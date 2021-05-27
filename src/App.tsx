de37d77d46bccea2b1bb6346ef4c481934ca7b4b
Alec Synnestvedt <aleccsyn@gmail.com>
2021-05-27 02:40:27 -0400

    updates

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
+import Comments from './components/comments';
 import './App.css';
 
 const App: React.FunctionComponent = () => {
   return (
     <div className="App">
       <div className="verticalCenter">
+        <p>WFH</p>
+        <Comments />
       </div>
     </div>
   );
---
+++ src/components/comments.ts
@@ -0,0 +1,16 @@
+import React from 'react';
+
+class Comments extends React.Component<{},{}> {
+  constructor(props:{}) {
+    super(props)
+      this.state = {
+        time: new Date()
+      }
+  }
+
+  render() {
+    return null;
+  }
+}
+
+export default Comments;
